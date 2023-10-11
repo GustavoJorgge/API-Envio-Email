7571fa8efd564c8de157423e21536bf56767fafd
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2023-10-10 21:17:26 -0300

    Iniciando Desenvolvimento Back-end

---
+++ Back-end/APIDisparoEmail/src/main/java/envio/email/APIDisparoEmail/ApiDisparoEmailApplication.java
@@ -0,0 +1,13 @@
+package envio.email.APIDisparoEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDisparoEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDisparoEmailApplication.class, args);
+	}
+
+}
---
+++ Back-end/APIDisparoEmail/src/test/java/envio/email/APIDisparoEmail/ApiDisparoEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package envio.email.APIDisparoEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDisparoEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
