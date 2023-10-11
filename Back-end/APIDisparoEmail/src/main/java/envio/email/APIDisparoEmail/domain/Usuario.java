3f73df020531d97020a3d300de85730e1a939347
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2023-10-10 21:59:50 -0300

    Adicionando Spring DataJPA

---
+++ Back-end/APIDisparoEmail/src/main/java/envio/email/APIDisparoEmail/domain/Usuario.java
@@ -0,0 +1,12 @@
+package envio.email.APIDisparoEmail.domain;
+
+import javax.annotation.processing.Generated;
+
+public class Usuario {
+    private Long id;
+    private String primeiroNome;
+    private String sobreNome;
+    private String email;
+    private String senha;
+
+}
