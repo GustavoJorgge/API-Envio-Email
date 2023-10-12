1d4cbf792f842b6d52a53f519a2bc491fb1c1681
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2023-10-12 19:46:26 -0300

    incluindo formulario

--- Back-end/APIDisparoEmail/src/main/java/envio/email/APIDisparoEmail/Controller/UsuarioController.java
+++ Back-end/APIDisparoEmail/src/main/java/envio/email/APIDisparoEmail/Controller/UsuarioController.java
@@ -9,12 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("cadastro")
 public class UsuarioController {
 
